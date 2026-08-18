7a8c2ad88fb175325447c8772afb598507f6efa9
Developer <dev@example.com>
2026-08-18 12:43:29 +0100

    Add Vercel and Web support

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Installing Flutter SDK..."
+git clone https://github.com/flutter/flutter.git -b stable
+export PATH="$PATH:`pwd`/flutter/bin"
+
+echo "Building Flutter Web App..."
+flutter config --enable-web
+flutter pub get
+flutter build web --release
